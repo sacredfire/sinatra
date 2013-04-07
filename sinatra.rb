a7de5861da49431a783129c9cdd8c87029bee928
Sergei Kissel <sergei.kissel@gmail.com>
2013-04-07 23:58:58 +0300

    Initial commit

---
+++ sinatra.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/about' do
+  'A little about me'
+end
+
+get '/hello/:name/' do
+  "Hi there #{params[:name].capitalize}"
+end
+
+get '/hello/:name/:city' do
+  "Hi there #{params[:name].capitalize} from #{params[:city].capitalize}"
+end
+
+get '/more/*' do
+  params[:splat]
+end
